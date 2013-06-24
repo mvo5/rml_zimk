ebada0c77b5e8c13277519be7f6a846e9ef6e0e6
Michael Vogt <mvo@debian.org>
2013-06-24 08:50:54 +0200

    add memory leak test

--- test/test_userblacklist.c
+++ test/test_userblacklist.c
@@ -5,34 +5,72 @@
 #include "zimk_userblacklist.h"
 
 
-static const char *blacklist_filename = "user_blacklist.cfg";
-
-void test_userblacklist_setup(void)
+long get_memory_usage(void)
 {
+   // getrusage() seems to not work properly, so we use statm (see man(8) proc)
+   FILE *f = fopen("/proc/self/statm", "r");
+   unsigned long size, tmp;
+   // get the VmSize
+   if(!fscanf(f, "%ld %ld %ld %ld %ld %ld %ld %ld",
+              &size, &tmp, &tmp, &tmp, &tmp, &tmp, &tmp, &tmp))
+      return -1;
+   fclose(f);
+   return size;
+}
+
+void test_userblacklist_setup(const char *blacklist_filename)
+{
+   int i;
+   char s[255];
+
    FILE *f = fopen(blacklist_filename, "w");
-   fputs("foo\n", f);
+   for (i=0;i<1000;i++) {
+      snprintf(s, sizeof(s)-1, "foo-%i\n", i);
+      fputs(s, f);
+   }
    fclose(f);
 }
 
-void test_userblacklist_teardown(void)
+void test_userblacklist_teardown(const char *blacklist_filename)
 {
    unlink(blacklist_filename);
 }
 
 void test_userblacklist(void)
 {
-   test_userblacklist_setup();
+   int i;
+   long mem_size = 0;
+   char blacklist_filename[255];
 
-   /* this user is in the blacklist */
-   g_assert_cmpint(TRUE,
-                    ==, 
-                    zimk_username_in_blacklist(blacklist_filename, "foo"));
-   /* and this one is not */
-   g_assert_cmpint(FALSE,
-                    ==, 
-                    zimk_username_in_blacklist(blacklist_filename, "bar"));
+   // run in a loop for memory size debugging
+   for(i=0;i<100;i++)
+   {
+      //printf("%ld\n", get_memory_usage());
 
-   test_userblacklist_teardown();
+      // we need a new filename to force re-loading the blacklist
+      snprintf(blacklist_filename, sizeof(blacklist_filename),
+               "user_blacklist-%i.cfg", i);
+      
+      test_userblacklist_setup(blacklist_filename);
+
+      /* this user is in the blacklist */
+      g_assert_cmpint(TRUE,
+                      ==, 
+                      zimk_username_in_blacklist(blacklist_filename, "foo-0"));
+      /* and this one is not */
+      g_assert_cmpint(FALSE,
+                      ==, 
+                      zimk_username_in_blacklist(blacklist_filename, "bar"));
+      
+      test_userblacklist_teardown(blacklist_filename);
+
+      // store the mem size after the first successfull run
+      if (mem_size == 0)
+         mem_size = get_memory_usage();
+   }
+   // ensure the mem size is not growing
+   g_assert_cmpint(mem_size, ==, get_memory_usage());
+
 }
 
 
--- zimk_userblacklist.c
+++ zimk_userblacklist.c
@@ -15,10 +15,19 @@
 
 GTree *blacklisted_users = NULL;
 time_t blacklisted_users_mtime = 0;
+char *blacklisted_users_cached_filename = NULL;
 
 static gboolean
-read_blacklisted_users(const char *blacklist_file, GTree *search_tree)
+read_blacklisted_users(const char *blacklist_file, GTree **blacklisted_users)
 {
+   if(*blacklisted_users)
+      g_tree_destroy(*blacklisted_users);
+   
+   *blacklisted_users = g_tree_new_full ((GCompareDataFunc)g_strcmp0,
+                                         NULL,
+                                         g_free,
+                                         NULL);
+
    char line[255];
    FILE *f = fopen(blacklist_file, "r");
    if (f == NULL)
@@ -30,16 +39,16 @@ read_blacklisted_users(const char *blacklist_file, GTree *search_tree)
 
    while(fgets(line, sizeof(line), f) != NULL) {
       g_strstrip(line);
-      g_tree_insert(search_tree, strdup(line), "1");
+      g_tree_insert(*blacklisted_users, strdup(line), "1");
    }
    fclose(f);
 
    return TRUE;
-   
 }
 
-gboolean zimk_username_in_blacklist(const char *blacklist_file,
-                               const char *username)
+gboolean
+zimk_username_in_blacklist(const char *blacklist_file,
+                           const char *username)
 {
    struct stat buf;
 
@@ -51,18 +60,17 @@ gboolean zimk_username_in_blacklist(const char *blacklist_file,
    }
 
    // update tree if needed
-   if (buf.st_mtime != blacklisted_users_mtime) 
+   if (buf.st_mtime != blacklisted_users_mtime ||
+       g_strcmp0(blacklist_file, blacklisted_users_cached_filename) != 0)
    {
-      if(blacklisted_users)
-         g_tree_destroy(blacklisted_users);
+      // (re)read blacklist
+      read_blacklisted_users(blacklist_file, &blacklisted_users);
 
-      blacklisted_users = g_tree_new_full ((GCompareDataFunc)g_strcmp0,
-                                           NULL,
-                                           g_free,
-                                           NULL);
-      read_blacklisted_users(blacklist_file, blacklisted_users);
-
+      // update the cache info
       blacklisted_users_mtime = buf.st_mtime;
+      if(blacklisted_users_cached_filename)
+         g_free(blacklisted_users_cached_filename);
+      blacklisted_users_cached_filename = g_strdup(blacklist_file);
    }
 
    // lookup user from blacklist
